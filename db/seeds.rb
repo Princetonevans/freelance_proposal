5285a0ebda76a6fb0146661928119fa4b9fb31f0
Princeton Evans <princetonevans@yahoo.com>
2018-09-03 08:42:38 -0500

    added database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#     10.times do |proposal|
+#     Proposal.create!(
+#          customer: "Customer #{proposal}",
+#          portfolio_url: 'http://princetonevans.com',
+#          tools: 'Angular 6, Ruby on Rails, and Postgres ',
+#          estimated_hours: (80 + proposal),
+#          hourly_rate: 120,
+#          weeks_to_complete: 12,
+#          client_email: 'Princetonevans@yahoo.com',
+#     )
+#     end
+      
+
+# puts 'db seeded'
+
+# rails g scaffold Proposal customer:string portfolo_url:string tools:string estimated_hours:decimal hourly_rate:decimal weeks_to_complete:integer client_email:string
